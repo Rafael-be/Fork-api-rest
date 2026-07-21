a91dfc0e0b232225c7b30283c3c2203f79da311c
Rafael-be <rafael.nascimento.be@gmail.com>
2026-07-21 10:07:50 -0300

    SSL tem que autorizar todos para rodar devido ao node

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: { rejectUnauthorized: true },
-  waitForConnections: true,
+  waitForConnections: false,
   connectionLimit: 10,
   queueLimit: 0
 });
