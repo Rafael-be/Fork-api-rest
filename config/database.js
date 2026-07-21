e4314fbe3571c65064ca3d32c757667f8745254b
Rafael-be <rafael.nascimento.be@gmail.com>
2026-07-21 10:04:22 -0300

    Teste para negar SSL nao autorizado

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: true },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
